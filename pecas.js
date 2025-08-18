78c47120cc1ae9d29fe8d81a5c52faf9eab24a51
Pablo Rachid de Bem <rachidpablodebem@gmail.com>
2025-08-18 18:38:03 -0300

    lógica de movimento concluida (aparentemente)

--- pecas.js
+++ pecas.js
@@ -10,44 +10,143 @@ const tabuleiro = [
 ];
 
 function trocar(origem, destino) {
-
+    //não podem ser posições nulas
     if (tabuleiro[origem] === '') return;
     if (tabuleiro[destino] !== '') return;
-
+    //onde estava vazio fica a peça; e onde estava a peça fica vazio
     tabuleiro[destino] = tabuleiro[origem];
-    tabuleiro[origem] = '';
+    tabuleiro[origem] = 'v';
 }
 
-export function mover(iPecinhaA, jPecinhaA, iPecinhaB, jPecinhaB) { 
+export function mover(iPecinhaA, jPecinhaA, iPecinhaB, jPecinhaB) {
+    //nome provisorio, para funcionar a é peça e b esta vazio 
     const pecinhaA = tabuleiro[iPecinhaA][jPecinhaA];
     const pecinhaB = tabuleiro[iPecinhaB][jPecinhaB];   
+
+    //validações 
+
+    //determina a direção, horizontal ou vertical
+    const direcao = decideDirecao(iPecinhaA, jPecinhaA, iPecinhaB, jPecinhaB);
+    // erro no calculo da direcao implica movimento invalido
+    if(direcao === "invalido")
+        return;
+    //calcula o sentido do movimento (esquerda, direita, baixo, cima)
+    const sentido = deciodeSentido(iPecinhaA, jPecinhaA, iPecinhaB, jPecinhaB, direcao);
+    // verifica se a distancia entre as duas posições é 2
+    const distanciaValida = distanciaValida(iPecinhaA, jPecinhaA, iPecinhaB, jPecinhaB, direcao);
+    //distancia invalida implica movimento invalido 
+    if(!distanciaValida)
+        return;
+    // verifica se tem uma peça entre as duas posições
+    const meioValida = validaMeio(iPecinhaA, jPecinhaA, iPecinhaB, jPecinhaB, direcao, sentido);
+    //se não tiver, movimento é invalido
+     if(!meioValida)
+        return;
+    
     
     if (pecinhaA === 'p') {
-        if (pecinhaB === 'v' && disanciaValida((iPecinhaA, jPecinhaA, iPecinhaB, jPecinhaB)){
-            trocar(iPecinhaA, iPecinhaB);
+        if (pecinhaB === 'v'){
+            trocar(iPecinhaA, jPecinhaA, iPecinhaB, jPecinhaB);
         }
-        else {
-            trocar(iPecinha, iPecinha + 1);
-        }
-        return;
     }
+}
 
-    if (tabuleiro[iPecinha] === 'p') {
-        if (tabuleiro[iPecinha - 1] !== '') {
-            trocar(iPecinha, iPecinha - 2);
-        }
-        else {
-            trocar(iPecinha, iPecinha - 1);
-        }
-        return;
+
+function decideDirecao(iPecinhaA, jPecinhaA, iPecinhaB, jPecinhaB)
+{
+    if(iPecinhaA === iPecinhaB)
+        return "horizontal";
+    if(jPecinhaA === jPecinhaB)
+        return "vertical";
+    return "invalido";
+
+}
+
+function distanciaValida(iPecinhaA, jPecinhaA, iPecinhaB, jPecinhaB, direcao)
+{
+    // se horizontal
+    if(direcao === "horizontal")
+    {
+        // valido se distancia = 2
+        if( Math.abs(jPecinhaA-jPecinhaB) === 2)
+            return true;
+        else
+            return false;
+
+    }
+    //senao, é vertical
+    else
+    {
+         if(Math.abs(iPecinhaA-iPecinhaB) === 2)
+            return true;
+        else
+            return false;
     }
 }
-function distanciaValida(iPecinhaA, jPecinhaA, iPecinhaB, jPecinhaB)
+
+function validaMeio(iPecinhaA, jPecinhaA, direcao, sentido)
 {
-    if( || )
-        return true;
-    return false;
+    if(direcao === "horizontal")
+    {
+        if(sentido === "esquerda")
+        {
+            if(tabuleiro[iPecinhaA][jPecinhaA-1] === "p")
+                return true;
+            else
+                return false;
+        }
+        else
+        {
+            if(tabuleiro[iPecinhaA][jPecinhaA+1] === "p")
+                return true;
+            else
+                return false;
+        }    
+    }
+    //vertical
+    else
+    {
+        if(sentido === "baixo")
+        {
+             if(tabuleiro[iPecinhaA-1][jPecinhaA] === "p")
+                return true;
+            else
+                return false;
+        }
+        else
+        {
+            if(tabuleiro[iPecinhaA+1][jPecinhaA] === "p")
+                return true;
+            else
+                return false;
+        }   
+    }    
 }
+
+function deciodeSentido(iPecinhaA, jPecinhaA, iPecinhaB, jPecinhaB, direcao )
+{
+    // se horizontal
+    if(direcao === "horizontal")
+    {
+        // valido se distancia = 2
+        if( jPecinhaA > jPecinhaB)
+            return "esquerda";
+        else
+            return "direita";
+    }
+    //senao, é vertical
+    else
+    {
+         if(iPecinhaA > iPecinhaB)
+            return "baixo";
+        else
+            return "cima";
+    }
+}
+
+
+
+
 export function getTabuleiro() {
     return structuredClone(tabuleiro);
 }
